81fa8ffe6993110b0aa5d2c0f290420a45b5e3c7
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2023-09-13 01:29:17 -0300

    Attempt to animate mealy machine example

--- mealy.py
+++ mealy.py
@@ -1,6 +1,6 @@
 from manim import *
 
-from mealy_machine_library import function_type, mealy_machine_as_a_function
+from mealy_machine_library import function_type, mealy_machine_as_a_function, play_automaton_as_animation
 
 
 class Introduction(Scene):
@@ -98,72 +98,6 @@ def mealy_machine_tuple(scene: Scene):
         current_element = element
 
 
-class MealyMachineExample(Scene):
+class MealyMachineExample(MovingCameraScene):
     def construct(self):
-        # Defina seus estados, transições e funções de saída aqui
-        states = ["q_0", "q_1"]
-        transitions = [((0, "q0"), "q1"), ((1, "q0"), "q1"), ((0, "q1"), "q0"), ((1, "q1"), "q0")]
-        outputs = [((0, "q0"), "a"), ((1, "q0"), "b"), ((0, "q1"), "a"), ((1, "q1"), "a")]
-
-        input_string = "0110"
-        output_string = ""
-
-        states_tex = [Tex(estado) for estado in states]
-        states_group = VGroup(*states_tex)
-        states_group.arrange(RIGHT, buff=1.5)
-
-        current_state_tex = Tex("q_0", color=GREEN)
-        current_state_tex.next_to(states_tex[0], UP)
-
-        self.play(Create(states_group))
-        self.play(Create(current_state_tex))
-
-        output_symbols_group = VGroup()  # Grupo para armazenar os símbolos de saída
-        arrows_group = VGroup()  # Grupo para armazenar as setas de transição
-
-        proximo_estado: str | None = None
-
-        for simbolo in input_string:
-            for transicao in transitions:
-                if (int(simbolo), current_state_tex.get_text()) == transicao[0]:
-                    proximo_estado = transicao[1]
-                    break
-
-            # Atualize o estado atual
-            proximo_estado_text = Text(proximo_estado, color=GREEN)
-            proximo_estado_text.next_to(states_tex[states.index(proximo_estado)], UP)
-
-            # Crie uma seta para representar a transição
-            seta = Arrow(current_state_tex, proximo_estado_text, buff=0.1)
-            arrows_group.add(seta)
-
-            # Determine a saída com base na função de saída
-            for saida in outputs:
-                if (int(simbolo), current_state_tex.get_text()) == saida[0]:
-                    simbolo_saida = saida[1]
-                    break
-
-            # Atualize a palavra de saída
-            output_string += simbolo_saida
-
-            # Crie animações para mostrar a saída na tela
-            simbolo_saida_text = Text(simbolo_saida)  # Alinhamento na parte inferior
-            if output_symbols_group:
-                simbolo_saida_text.next_to(output_symbols_group, RIGHT, buff=0.2)  # Espaçamento entre símbolos
-            else:
-                simbolo_saida_text.next_to(current_state_tex, DOWN, buff=0.2)  # Espaçamento entre símbolos
-            output_symbols_group.add(simbolo_saida_text)
-
-            # Animação de transição e saída
-            self.play(
-                Transform(current_state_tex, proximo_estado_text),
-                Create(seta),
-                Create(simbolo_saida_text)
-            )
-            self.wait(0.5)
-
-        # Exiba a palavra de saída final
-        palavra_saida_texto = Text(output_string)  # Alinhamento na parte inferior
-        palavra_saida_texto.next_to(current_state_tex, DOWN, buff=0.2)  # Espaçamento entre símbolos
-        self.play(Create(palavra_saida_texto))
-        self.wait()
+        play_automaton_as_animation("./mealy-example.jff", self)
