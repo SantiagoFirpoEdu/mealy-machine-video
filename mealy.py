3baf8ca3a791af9307119b5c035a030709c65dd2
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2023-09-14 00:53:22 -0300

    Started working on example

--- mealy.py
+++ mealy.py
@@ -45,14 +45,13 @@ class MealyMachineDefinition(Scene):
     def construct(self):
         self.finite_transductor_title()
 
-        self.same_length_property()
 
         # Create the text and shape
         mealy_machine_as_a_function(self)
 
         mealy_machine_tuple(self)
 
-        transition_function_definition = Tex(function_type("sigma", "Q \times Sigma", "Q"))
+        self.same_length_property()
 
     def same_length_property(self):
         input_word: Tex = Tex("$w_0$")
@@ -77,27 +76,50 @@ class MealyMachineDefinition(Scene):
 
 
 def mealy_machine_tuple(scene: Scene):
-    empty_tuple = Tex("$()$")
-    tuple_1_element = Tex("$(Q)$")
-    tuple_2_element = Tex("$(Q, \Sigma)$")
-    tuple_3_element = Tex("$(Q, \Sigma, q_i)$")
-    tuple_4_element = Tex("$(Q, \Sigma, q_i, \Delta)$")
-    tuple_5_element = Tex("$(Q, \Sigma, q_i, \Delta, \sigma)$")
-    tuple_6_element = Tex("$(Q, \Sigma, q_i, \Delta, \sigma, \Lambda)$")
+    empty_tuple: Tex = Tex("$()$")
+    tuple_1_element: Tex = Tex("$(Q)$")
+    tuple_2_element: Tex = Tex("$(Q, \Sigma)$")
+    tuple_3_element: Tex = Tex("$(Q, \Sigma, \Gamma)$")
+    tuple_4_element: Tex = Tex("$(Q, \Sigma, \Gamma, q_i)$")
+    tuple_5_element: Tex = Tex("$(Q, \Sigma, \Gamma, q_i, \delta)$")
+    tuple_6_element: Tex = Tex("$(Q, \Sigma, \Gamma, q_i, \delta, \Lambda)$")
 
-    tuple_elements = [tuple_1_element, tuple_2_element, tuple_3_element, tuple_4_element, tuple_5_element,
+    transition_function_definition = Tex(function_type("\sigma", "Q \times Sigma", "Q"))
+
+
+    delta_definition: Tex = Tex(function_type("\sigma", "Q \\times \Sigma", "Q"))
+    lambda_definition: Tex = Tex(function_type("\lambda", "Q \\times \Sigma", "\Gamma"))
+
+    function_definitions: dict[int, Tex] = {
+        5: delta_definition,
+        6: lambda_definition
+    }
+
+    tuple_elements: list[Tex] = [tuple_1_element, tuple_2_element, tuple_3_element, tuple_4_element, tuple_5_element,
                       tuple_6_element]
 
     scene.play(Write(empty_tuple))
 
     current_element = empty_tuple
+    current_function_definition: Tex | None = None
 
-    for element in tuple_elements:
-        scene.wait(2)
-        scene.play(ReplacementTransform(current_element, element))
+    for element, index in tuple_elements:
+        if function_definitions.has_key(index):
+            function_definition = function_definitions[index]
+            scene.wait(2)
+            if function_definition is not None:
+                scene.play(function_definition)
+            scene.play(ReplacementTransform(current_element, element))
+            current_function_definition = function_definition
         current_element = element
 
 
 class MealyMachineExample(MovingCameraScene):
     def construct(self):
-        play_automaton_as_animation("./mealy-example.jff", self)
+        sample_mealy_machine_animation(self)
+
+def sample_mealy_machine_animation(scene: Scene):
+    q0_state = Tex("$q_0$")
+    initial_state_marker = Triangle().rotate(60 * DEGREES)
+
+    q1_state = Tex("$q_1$")
